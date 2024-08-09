3eeb2c2db95e4c2b9e200db59d44839038173bf8
pedroruzenent <161774234+pedroruzenent@users.noreply.github.com>
2024-08-08 21:34:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,71 +6,72 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Você está em uma nave espacial que acaba de pousar em um planeta desconhecido. Qual é a sua primeira reação?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Estou animado para explorar!",
-                afirmacao: "Você decide imediatamente sair da nave para começar a explorar o planeta."
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Estou com medo do desconhecido.",
-                afirmacao: "Você opta por ficar dentro da nave e analisar os dados do planeta antes de sair."
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
     },
     {
-        enunciado: "Durante a exploração, você encontra uma estrutura alienígena. Como você procede?",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Investiga a estrutura usando o equipamento de pesquisa da nave.",
-                afirmacao: "Você coleta amostras e dados valiosos sobre a estrutura alienígena."
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
             },
             {
-                texto: "Decide não se aproximar e manter distância para evitar riscos.",
-                afirmacao: "Você opta por analisar a estrutura de longe e enviar os dados para a Terra."
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
             }
         ]
     },
     {
-        enunciado: "Ao retornar à nave, você discute com sua equipe sobre o impacto das descobertas. Qual é sua opinião?",
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-                texto: "A descoberta pode abrir novas oportunidades para a exploração espacial.",
-                afirmacao: "Você acredita que as novas informações podem levar a futuras explorações e descobertas."
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
             },
             {
-                texto: "Estou preocupado com os riscos que podem surgir com essas descobertas.",
-                afirmacao: "Você sugere precauções adicionais e uma análise mais aprofundada dos riscos."
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
             }
         ]
     },
     {
-        enunciado: "Você precisa criar um relatório detalhado sobre a missão para apresentar na conferência espacial. Como você faz isso?",
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
             {
-                texto: "Usa um software de apresentação para criar um relatório visualmente atraente.",
-                afirmacao: "Você produz um relatório bem elaborado e apresenta os resultados de forma clara."
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
             },
             {
-                texto: "Escreve o relatório manualmente e detalha todas as observações e descobertas.",
-                afirmacao: "Você fornece um relatório completo e detalhado, abordando cada aspecto da missão."
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
             }
         ]
     },
     {
-        enunciado: "Você e sua equipe têm uma nova missão programada para o próximo mês. Como se prepara para isso?",
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
         alternativas: [
             {
-                texto: "Participa de treinamentos e simulações para se preparar para os desafios da nova missão.",
-                afirmacao: "Você e sua equipe estão bem preparados e atualizados para enfrentar novos desafios."
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
             },
             {
-                texto: "Revisa os dados da missão anterior e ajusta os equipamentos com base nas lições aprendidas.",
-                afirmacao: "Você garante que todos os equipamentos estão em perfeitas condições e aprende com a experiência passada."
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
             }
         ]
     },
 ];
+
 
 let atual = 0;
 let perguntaAtual;
@@ -104,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "E assim terminou sua missão...";
+    caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
